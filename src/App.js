81da7205dc88bbb9b56ce1fb99383cdc73111b3d
Rolanda-Davids-97 <davidsrolanda@yahoo.com>
2024-04-25 21:41:11 +0200

    added my open source code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ export default function App() {
     <div className="App">
       <h1>WEATHER APP</h1>
       <Weather />
+      <p>
+        <small>
+          <a
+            href="https://github.com/Rolanda-Davids-97/weather-react"
+            target="-blank"
+          >
+            Open-source code
+          </a>
+          , by Rolanda Davids{" "}
+        </small>
+      </p>
     </div>
   );
 }
